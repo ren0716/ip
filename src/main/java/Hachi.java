92ac7a41d5c8645cfd4879b846dde4317c6e4e8c
ren0716 <behsr2003@gmail.com>
2025-08-28 19:34:45 +0800

    separate logic for UI, parsing, Tasklist using OOP

--- src/main/java/Hachi.java
+++ src/main/java/Hachi.java
@@ -1,229 +1,31 @@
 import java.io.File;
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.time.LocalDateTime;
-import java.util.regex.*;
-enum Command {
-    LIST, BYE, MARK, UNMARK, TODO, DEADLINE, EVENT, UNKNOWN, DELETE;
-
-    static Command from(String input) {
-        if (input == null || input.isBlank()) return UNKNOWN;
-        String first = input.trim().split("\\s+", 2)[0].toUpperCase();
-        try {
-            return Command.valueOf(first);
-        } catch (IllegalArgumentException e) {
-            return UNKNOWN;
-        }
-    }
-}
 
 public class Hachi {
-    public static void main(String[] args) {
+    private TaskList tasks;
+    private File file;
+    private Storage storage;
+    private Ui ui;
 
-        String logo =
-                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠄⠤⠤⢄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠀⣠⠔⢒⠟⠉⠑⠀⠀⠀⠀⠀⠉⠣⡀⠙⣂⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⢀⠗⠀⢗⠂⠀⠀⠀⠀⠀⠀⠀⠀⠈⡏⠀⠈⢢⡀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⢸⠂⢰⣶⡄⠠⠤⠴⠿⣇⢀⣹⡀⠀⠈⢣⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠰⡀⠀⠀⢨⠦⠉⠁⠀⢾⣿⡆⠀⠀⠈⠉⣃⠀⢀⠟⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠙⢧⣀⡰⡑⡀⠀⠤⠴⠛⢲⠒⠀⢠⢸⠿⠾⠋⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠏⠑⠧⢇⣄⢈⡒⣊⣄⠼⠚⠙⢥⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠀⣠⢺⠂⠀⠀⠠⡀⠉⠁⠀⠀⠀⠀⠀⢪⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⡸⠁⠘⡆⠀⠀⠈⠇⡀⠀⠀⠀⡂⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⣦⣀⣀⣇⠀⠀⢣⡀⠀⠀⠈⠣⣀⠀⣄⠃⠀⠀⡟⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠑⢅⣠⡃⣀⣀⡀⠁⠀⠀⠀⠀⢫⢹⠁⠀⠀⠀⢦⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⢹⢩⢀⢀⣈⡇⠀⠀⠀⠀⢸⡘⠄⠀⢀⠀⢈⡅⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠈⠚⠒⠓⠒⠋⠀⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "      _   _            _     _ \n" +
-                        "     | | | | __ _  ___| |__ (_)\n" +
-                        "     | |_| |/ _` |/ __| '_ \\| |\n" +
-                        "     |  _  | (_| | (__| | | | |\n" +
-                        "     |_| |_|\\__,_|\\___|_| |_|_|\n";
-
-        String exit =
-                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⡤⠒⠋⠓⠋⠉⠉⠉⠉⠒⢦⣀⡀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀\n" +
-                        "⠀⠀⢀⡤⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢣⡀⠀⢀⡤⠞⠉⠉⠙⡆⠀⠀\n" +
-                        "⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠀⠀⠀⠳⣄⣏⠀⠀⠀⠀⠀⠈⢳⡀\n" +
-                        "⢀⡏⠀⠀⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢸⢸⡀⠀⠀⠀⠀⠀⡼⠁\n" +
-                        "⠀⡇⠀⢀⠗⠀⢠⣤⡀⠀⠀⢀⣀⡀⠀⠃⠀⠀⠀⠀⠘⡾⢄⡀⠀⠀⠀⣠⠇⠀\n" +
-                        "⠀⠙⢤⣼⠆⠀⠈⠉⣤⣤⡄⠘⠛⠃⠀⢱⠀⠀⠀⢀⡰⠃⠀⠈⠒⠑⢺⡀⠀⠀\n" +
-                        "⠀⠀⠀⠘⢇⡀⠀⠢⡬⠯⣀⡀⠀⠀⠀⠀⢓⠄⠐⠁⠀⠀⠀⠀⠀⠀⠀⢳⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⢯⠢⢄⡈⠉⠀⠀⠀⣀⣀⡠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠸⡀⠀⠈⠉⠉⠁⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠂⠀⠀⢠⠇⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⢹⠢⡄⠀⠀⠀⡀⠀⠀⠀⠀⢀⠀⠀⠀⠀⢀⠌⠀⠀⠀⠸⡀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⡸⠀⠈⠒⢂⣄⠇⠀⠀⠀⠀⡸⠤⢔⡶⠊⠉⢳⡄⠀⠄⠀⡇⠀\n" +
-                        "⠀⠀⠀⠀⠀⢰⠃⠀⠀⢀⡞⡼⠀⠀⠀⠀⢀⡟⠚⠋⠀⠀⠀⢯⣄⣄⣠⠞⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠈⠛⠓⠒⠋⠸⣠⣠⣀⣀⡖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n";
-
-
-        String separation = "_________________________________________________";
-
-        String cmd = "";
-        Scanner scanner = new Scanner(System.in);
-        File f = new File("output/output.txt");
-        ArrayList<Task> tasks = new ArrayList<>();
-        Storage storage = new Storage(f);
-
+    public Hachi(String filePath) {
+        file = new File(filePath);
+        storage = new Storage(file);
         try {
-            tasks = storage.unpack();
-            System.out.println(tasks);
+            tasks = new TaskList(storage.unpack());
+            System.out.println(tasks.tasks);
 
         } catch (IOException e) {
             System.out.println("Hachi has no prior memories");
+            tasks = new TaskList();
         }
+        file = new File(filePath);
+    }
 
-        System.out.println("Woof! Nice to meet you" + logo + "\n" + separation);
+    public void run() {
+        Parser.start(tasks, storage);
+    }
 
-        while (true) {
-            cmd = scanner.nextLine();
-            Command command = Command.from(cmd);
-
-            switch (command) {
-                case LIST:
-                    for (int i = 0; i < tasks.size(); i++) {
-                        Task current = tasks.get(i);
-                        System.out.println((i + 1) + ". " + current.toString());
-                    }
-                    break;
-
-                case BYE:
-                    try {
-                        storage.write(tasks);
-                    } catch (IOException e) {
-                        System.out.println("file not storage");
-                    }
-                    System.out.println("Come play again!" + exit + "\n" + separation);
-                    return;
-
-                case UNMARK: {
-                    String[] parts = cmd.split(" ");
-                    if (parts.length > 1) {
-                        int taskNumber = Integer.parseInt(parts[1]);
-                        if (taskNumber >= 1 && taskNumber <= tasks.size()) {
-                            tasks.get(taskNumber - 1).unmark();
-                            System.out.println("🐶 Hachi whines softly: 'Did we bark too soon?'\n" + separation);
-                        } else {
-                            System.out.println("🐶 Hachi sniffed everywhere, but no task found with that number.");
-                            System.out.println("Maybe fetch another number?\n" + separation);
-                        }
-                    } else {
-                        System.out.println("🐶 Hachi is confused: 'which task should i unmark?'" + "\n" + separation);
-                    }
-                    break;
-                }
-
-                case MARK: {
-                    String[] parts = cmd.split(" ");
-                    if (parts.length > 1) {
-                        int taskNumber = Integer.parseInt(parts[1]);
-                        if (taskNumber >= 1 && taskNumber <= tasks.size()) {
-                            tasks.get(taskNumber - 1).mark();
-                            System.out.println("🐶 Hachi wags his tail proudly: 'Task complete!'\n" + separation);
-                        } else {
-                            System.out.println("🐶 Hachi sniffed everywhere, but no task found with that number.");
-                            System.out.println("Maybe fetch another number?\n" + separation);
-                        }
-                    } else {
-                        System.out.println("🐶 Hachi is confused: 'which task should i mark?'" + "\n" + separation);
-                    }
-                    break;
-                }
-
-                case TODO: {
-                    String[] parts = cmd.split(" ", 2);
-                    if (parts.length > 1) {
-                        tasks.add(new ToDo(parts[1]));
-                        System.out.println("🐕 Paw-some! I’ve added this task to the list:");
-                        System.out.println(tasks.get(tasks.size() - 1).toString());
-                        System.out.println(String.format("You now have (%d) task", tasks.size()) + "\n" + separation);
-                    } else {
-                        System.out.println("Woof! You can't just do nothing!" + "\n" + separation);
-                    }
-                    break;
-                }
-
-                case DEADLINE: {
-                    Pattern pattern = Pattern.compile(
-                            "^deadline\\s+(.*?)\\s+/by\\s+(\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4})$",
-                            Pattern.CASE_INSENSITIVE
-                    );
-                    Matcher matcher = pattern.matcher(cmd);
-
-                    if (matcher.matches()) {
-                        String task = matcher.group(1);
-                        String by = matcher.group(2);
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-                        LocalDateTime dateTime = LocalDateTime.parse(matcher.group(2), formatter);
-                        tasks.add(new Deadline(task, dateTime));
-                        System.out.println("🐕 Paw-some! I’ve added this task to the list:");
-
-                    } else {
-                        System.out.println
-                                ("🐶 Hachi paws at you:" +
-                                        " 'I need both a task and a deadline! Use it like: deadline <task> /by <time>'");
-                        System.out.println("Note: Input your time as d/M/yyyy HHmm so Hachi can understand" + "\n" + separation);
-                    }
-                    break;
-                }
-
-
-                case EVENT: {
-                    Pattern pattern = Pattern.compile(
-                            "^event\\s+(.*?)\\s+/from\\s+(\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4})\\s+/to\\s+(\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4})$",
-                            Pattern.CASE_INSENSITIVE
-                    );
-                    Matcher matcher = pattern.matcher(cmd);
-
-                    if (matcher.matches()) {
-                        String task = matcher.group(1);
-                        String fromStr = matcher.group(2);
-                        String toStr = matcher.group(3);
-
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-                        LocalDateTime from = LocalDateTime.parse(fromStr, formatter);
-                        LocalDateTime to = LocalDateTime.parse(toStr, formatter);
-
-                        tasks.add(new Event(task, from, to));
-                        System.out.println("🐕 Paw-some! I’ve added this task to the list:");
-                    } else {
-                        System.out.println(
-                                "🐶 Hachi tilts his head:" +
-                                        " 'I need a task, a start time, and an end time! Use it like: event <task> /from <start> /to <end>'"
-                        );
-                        System.out.println("Note: Input your time as d/M/yyyy HHmm so Hachi can understand"
-                                + "\n" + separation);
-
-                    }
-                    break;
-                }
-
-
-                case DELETE: {
-                    String[] parts = cmd.split(" ");
-                    if (parts.length > 1) {
-                        int taskNumber = Integer.parseInt(parts[1]);
-                        if (taskNumber >= 1 && taskNumber <= tasks.size()) {
-                            tasks.remove(taskNumber - 1);
-                            System.out.println("\uD83D\uDC36 \"Hachi dug a hole and buried that task. It’s gone!\"\n" + separation);
-                        } else {
-                            System.out.println("🐶 Hachi sniffed everywhere, but no task found with that number.");
-                            System.out.println("Maybe fetch another number?\n" + separation);
-                        }
-                    } else {
-                        System.out.println("🐶 Hachi is confused: 'which task should i remove?'" + "\n" + separation);
-                    }
-                    break;
-                }
-
-                case UNKNOWN:
-                default:
-                    System.out.println("🐶 Hachi tilts his head: 'I don’t understand that command.'\n" + separation);
-            }
-        }
+    public static void main(String[] args) {
+        new Hachi("output/output.txt").run();
     }
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,175 @@
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+enum Command {
+    LIST, BYE, MARK, UNMARK, TODO, DEADLINE, EVENT, UNKNOWN, DELETE;
+
+    static Command from(String input) {
+        if (input == null || input.isBlank()) return UNKNOWN;
+        String first = input.trim().split("\\s+", 2)[0].toUpperCase();
+        try {
+            return Command.valueOf(first);
+        } catch (IllegalArgumentException e) {
+            return UNKNOWN;
+        }
+    }
+}
+
+public class Parser {
+    private static final int TODO = 0;
+    private static final int DEADLINE = 1;
+    private static final int EVENT = 2;
+    private static final int MARK = 3;
+    private static final int UNMARK = 4;
+    private static final int DELETE = 5;
+    private static final int UNKNOWN = 6;
+    private static final int MISSING = 7;
+
+    public static void start(TaskList tasks, Storage storage) {
+
+
+        String separation = "_________________________________________________";
+        String cmd = "";
+        Scanner scanner = new Scanner(System.in);
+        Ui ui = new Ui(tasks);
+        Ui.start();
+
+        while (true) {
+            cmd = scanner.nextLine();
+            Command command = Command.from(cmd);
+
+            switch (command) {
+                case LIST:
+                    for (int i = 0; i < tasks.tasks.size(); i++) {
+                        Task current = tasks.tasks.get(i);
+                        System.out.println((i + 1) + ". " + current.toString());
+                    }
+                    break;
+
+                case BYE:
+                    try {
+                        storage.write(tasks.tasks);
+                    } catch (IOException e) {
+                        System.out.println("file not storage");
+                    }
+                    Ui.end();
+                    return;
+
+                case UNMARK: {
+                    String[] parts = cmd.split(" ");
+                    if (parts.length > 1) {
+                        int taskNumber = Integer.parseInt(parts[1]);
+                        if (taskNumber >= 1 && taskNumber <= tasks.tasks.size()) {
+                            tasks.tasks.get(taskNumber - 1).unmark();
+                            ui.success(UNMARK);
+                        } else {
+                            Ui.failure(UNMARK);
+                        }
+                    } else {
+                        Ui.failure(UNMARK);
+                    }
+                    break;
+                }
+
+                case MARK: {
+                    String[] parts = cmd.split(" ");
+                    if (parts.length > 1) {
+                        int taskNumber = Integer.parseInt(parts[1]);
+                        if (taskNumber >= 1 && taskNumber <= tasks.tasks.size()) {
+                            tasks.tasks.get(taskNumber - 1).mark();
+                            ui.success(MARK);
+                        } else {
+                            Ui.failure(MISSING);
+                        }
+                    } else {
+                        Ui.failure(MARK);
+                    }
+                    break;
+                }
+
+                case TODO: {
+                    String[] parts = cmd.split(" ", 2);
+                    if (parts.length > 1) {
+                        tasks.tasks.add(new ToDo(parts[1]));
+                        ui.success(TODO);
+                    } else {
+                        Ui.failure(TODO);
+                    }
+                    break;
+                }
+
+                case DEADLINE: {
+                    Pattern pattern = Pattern.compile(
+                            "^deadline\\s+(.*?)\\s+/by\\s+(\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4})$",
+                            Pattern.CASE_INSENSITIVE
+                    );
+                    Matcher matcher = pattern.matcher(cmd);
+
+                    if (matcher.matches()) {
+                        String task = matcher.group(1);
+                        String by = matcher.group(2);
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+                        LocalDateTime dateTime = LocalDateTime.parse(by, formatter);
+                        tasks.tasks.add(new Deadline(task, dateTime));
+                        ui.success(DEADLINE);
+
+                    } else {
+                        Ui.failure(DEADLINE);
+                    }
+                    break;
+                }
+
+
+                case EVENT: {
+                    Pattern pattern = Pattern.compile(
+                            "^event\\s+(.*?)\\s+/from\\s+(\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4})\\s+/to\\s+(\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4})$",
+                            Pattern.CASE_INSENSITIVE
+                    );
+                    Matcher matcher = pattern.matcher(cmd);
+
+                    if (matcher.matches()) {
+                        String task = matcher.group(1);
+                        String fromStr = matcher.group(2);
+                        String toStr = matcher.group(3);
+
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+                        LocalDateTime from = LocalDateTime.parse(fromStr, formatter);
+                        LocalDateTime to = LocalDateTime.parse(toStr, formatter);
+
+                        tasks.tasks.add(new Event(task, from, to));
+                        ui.success(EVENT);
+                    } else {
+                        Ui.failure(EVENT);
+
+                    }
+                    break;
+                }
+
+
+                case DELETE: {
+                    String[] parts = cmd.split(" ");
+                    if (parts.length > 1) {
+                        int taskNumber = Integer.parseInt(parts[1]);
+                        if (taskNumber >= 1 && taskNumber <= tasks.tasks.size()) {
+                            tasks.tasks.remove(taskNumber - 1);
+                            System.out.println("\uD83D\uDC36 \"Hachi dug a hole and buried that task. It’s gone!\"\n" + separation);
+                        } else {
+                            Ui.failure(MISSING);
+                        }
+                    } else {
+                        Ui.failure(DELETE);
+                    }
+                    break;
+                }
+
+                case UNKNOWN:
+                default:
+                    Ui.failure(UNKNOWN);
+            }
+        }
+    }
+}
